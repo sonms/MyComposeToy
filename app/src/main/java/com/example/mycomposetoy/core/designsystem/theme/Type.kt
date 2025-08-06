1cc881205788b8e0f2ee2ee1673da3c1659cd5f5
sonms <smsms5676@naver.com>
2025-08-06 21:28:03 +0900

    move/#1 designsystem으로 이동

--- app/src/main/java/com/example/mycomposetoy/core/designsystem/theme/Type.kt
+++ app/src/main/java/com/example/mycomposetoy/core/designsystem/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.mycomposetoy.ui.theme
+package com.example.mycomposetoy.core.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
