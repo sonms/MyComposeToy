191c896eaf0106feee93ea61fb97754a0adcddd2
sonms <smsms5676@naver.com>
2025-08-08 00:27:15 +0900

    feat/#5 basic text - auto size 기능

--- app/src/main/java/com/example/mycomposetoy/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/mycomposetoy/presentation/home/HomeScreen.kt
@@ -1,12 +1,27 @@
 package com.example.mycomposetoy.presentation.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.TextAutoSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeRoute(
@@ -19,14 +34,57 @@ fun HomeRoute(
 fun HomeScreen(
     modifier: Modifier = Modifier
 ) {
+    var text by remember {
+        mutableStateOf("")
+    }
+
     Column (
         modifier = modifier
             .fillMaxSize()
             .background(Color.White)
-    ){
+            .defaultPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
-            text = "home",
-            color = Color.Black
+            text = "텍스트를 입력하세요:"
         )
+
+        BasicTextField(
+            value = text,
+            onValueChange = { text = it },
+            modifier = Modifier,
+            decorationBox = { innerTextField ->
+                Box(
+                    modifier = Modifier
+                        .border(
+                            width = 2.dp,
+                            color = Color.Gray,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .padding(start = 12.dp, end = 12.dp, top = 8.dp, bottom = 8.dp)
+                ) {
+                    innerTextField()
+                }
+            }
+        )
+
+        Box {
+            // 컨테이너 크기에 자동 크기 조정으로 맞춤
+            BasicText(
+                text = "입력 텍스트: $text ",
+                maxLines = 1,
+                softWrap = true,
+                autoSize = TextAutoSize.StepBased(),
+                overflow = TextOverflow.Ellipsis,
+            )
+        }
     }
+}
+
+fun Modifier.defaultPadding() = padding(16.dp)
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen()
 }
