4ce91cbb9660d1223bf80d1dae9d1201287c4412
sonms <smsms5676@naver.com>
2025-08-13 21:04:01 +0900

    delete/#7 navigation 역할을 Route로 이동

--- app/src/main/java/com/example/mycomposetoy/presentation/user/navigation/UserListNavigation.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mycomposetoy.presentation.user.navigation
-
-import com.example.mycomposetoy.core.navigation.Route
-import kotlinx.serialization.Serializable
-
-@Serializable
-data object UserList : Route
