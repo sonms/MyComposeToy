ccfbced223fca895c5ebca37bb226a996f0803b1
sonms <smsms5676@naver.com>
2025-08-13 20:34:59 +0900

    feature/#7 user list data 구현 - datasource, impl, dto

---
+++ app/src/main/java/com/example/mycomposetoy/data/repositoryimpl/UserListRepositoryImpl.kt
@@ -0,0 +1,20 @@
+package com.example.mycomposetoy.data.repositoryimpl
+
+import com.example.mycomposetoy.data.datasource.remote.UserListDataSource
+import com.example.mycomposetoy.domain.entity.PaginatedUserListEntity
+import com.example.mycomposetoy.domain.repository.UserListRepository
+import javax.inject.Inject
+
+class UserListRepositoryImpl @Inject constructor(
+    private val dataSource: UserListDataSource
+) : UserListRepository {
+    override suspend fun getUserList(page: Int): Result<PaginatedUserListEntity> = runCatching {
+        val response = dataSource.getUserList(page)
+
+        PaginatedUserListEntity(
+            userList = response.data.map { it.toDomain() },
+            currentPage = response.page,
+            totalPages = response.totalPages
+        )
+    }
+}
