4b09937e57c8657fa652c161083118509cec942d
sonms <smsms5676@naver.com>
2025-08-13 20:37:55 +0900

    feature/#7 user Navigation 구현 - home에서 userList로 이동

---
+++ app/src/main/java/com/example/mycomposetoy/presentation/user/navigation/UserListNavigation.kt
@@ -0,0 +1,7 @@
+package com.example.mycomposetoy.presentation.user.navigation
+
+import com.example.mycomposetoy.core.navigation.Route
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object UserList : Route
