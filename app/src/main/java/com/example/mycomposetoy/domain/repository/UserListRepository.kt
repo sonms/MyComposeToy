2e9e323c83e6190463b5b518b4aab6579fc0e076
sonms <smsms5676@naver.com>
2025-08-15 00:54:04 +0900

    feat/#9 domain 구현 - usecase, repository, entity

--- app/src/main/java/com/example/mycomposetoy/domain/repository/UserListRepository.kt
+++ app/src/main/java/com/example/mycomposetoy/domain/repository/UserListRepository.kt
@@ -1,7 +1,10 @@
 package com.example.mycomposetoy.domain.repository
 
 import com.example.mycomposetoy.domain.entity.PaginatedUserListEntity
+import com.example.mycomposetoy.domain.entity.UserDetailEntity
 
 interface UserListRepository {
     suspend fun getUserList(page: Int) : Result<PaginatedUserListEntity>
+
+    suspend fun getUserDetail(id: Int) : Result<UserDetailEntity>
 }
